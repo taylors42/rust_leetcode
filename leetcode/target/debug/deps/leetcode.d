e64f8040ef1c4ac6464905a6fcf0f365f812cf9d
Taylor <96550502+taylors42@users.noreply.github.com>
2024-03-13 16:34:14 -0300

    Delete leetcode/target directory

--- leetcode/target/debug/deps/leetcode.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\target\debug\deps\leetcode.exe: src\main.rs
-
-C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\target\debug\deps\leetcode.d: src\main.rs
-
-src\main.rs:
--- leetcode/target/debug/leetcode.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\target\debug\leetcode.exe: C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\src\main.rs
