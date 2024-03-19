758cbece964a223758bbb121d570d6a98c5ebf85
Taylor <96550502+taylors42@users.noreply.github.com>
2024-03-18 22:23:52 -0300

    added a sucess solution for two_sum

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,16 +1,14 @@
-fn main() {
-    
-}
-
-
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut output: Vec<i32> = Vec::new();
-    for i in 1..nums.len(){
-    let index: i32 = i.try_into().unwrap();
-    if nums[i - 1] + nums[i] == target{
-        output.push(index - 1);
-        output.push(i.try_into().unwrap());
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut hashmap: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            let complement = target - nums[i];
+            if hashmap.contains_key(&complement) {
+                return vec![i as i32, *hashmap.get_key_value(&complement).unwrap().1];
+            }
+            hashmap.insert(nums[i], i as i32);
         }
+        vec![]
     }
-    output
 }
---
+++ leetcode/target/debug/deps/leetcode-0579641d29b51d76.d
@@ -0,0 +1,5 @@
+C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\target\debug\deps\libleetcode-0579641d29b51d76.rmeta: src\main.rs
+
+C:\Users\Tay\Documents\GitHub\rust_leetcode\leetcode\target\debug\deps\leetcode-0579641d29b51d76.d: src\main.rs
+
+src\main.rs:
